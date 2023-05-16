f0be9b780eb959a79980d22baba70a71c429eef7
chris-alaf <crsalaf@gmail.com>
2023-05-16 21:43:27 +0300

    Update main.java

--- Substract/src/sub/main.java
+++ Substract/src/sub/main.java
@@ -5,6 +5,6 @@ public class main {
 	private static substract subs= new substract();
 	
 	public static void main(String[] args) {
-		System.out.printf(subs.subt(10,5));
+		System.out.printf(subs.subt(5,10));
 	}
 }
